abdc926f3c0a7a5153f79d2ba72ccb704b002548
Matthew Ellis <mbellistx@gmail.com>
2021-04-09 16:12:26 -0500

    moved action middleware to actions-middleware.js

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,50 +2,14 @@
 const express = require('express')
 
 const actions = require('./actions-model.js')
-const projects = require('../projects/projects-model.js')
+
+const {
+  validateActionId,
+  validateAction
+} = require('./actions-middleware.js')
 
 const router = express.Router()
 
-function validateActionId(req, res, next){
-  actions.get(req.params.id)
-    .then( action => {
-      if(action){
-        req.action = action
-        next()
-      } else {
-        res.status(404).json({message: `Could not find action with id ${req.params.id}`})
-      }
-    })
-    .catch( error => {
-      res.status(500).json({message: 'Server error', error: error.toString()})
-    })
-}
-
-async function validateAction(req, res, next){
-  const { project_id, description, notes, completed } = req.body
-  // I'd like to reuse validateProjectId but I can't really do that without it being really jank
-  if(description && description.length < 128 && notes){
-    const project = await projects.get(project_id)
-    
-    if(project){
-      req.project = project // not needed but it's there if I do need it ig
-      
-      req.newAction = {
-        project_id,
-        description,
-        notes,
-        completed: completed === true ? 1 : 0
-      }
-      next()
-
-    } else {
-      res.status(404).json({message: `Could not find project with id ${project_id}`})
-    }
-
-  } else {
-    res.status(400).json({message: 'Description or notes missing, or description longer than 128 chars'})
-  }
-}
 
 router.get('/', (_, res) => {
   actions.get()
