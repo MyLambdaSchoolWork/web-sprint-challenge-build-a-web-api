0fa3431ce7b11f11c5d92692851f7df61646b080
Matthew Ellis <mbellistx@gmail.com>
2021-04-09 13:59:05 -0500

    C:/Program Files/Git/api/projects CRUD done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,85 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const projects = require('./projects-model.js')
+
+const router = express.Router()
+
+function validiateProjectId(req, res, next){
+  projects.get(req.params.id)
+    .then( project => {
+      if(project){
+        req.project = project
+        next()
+      } else {
+        res.status(404).json({message: `Could not find project with id ${req.params.id}`})
+      }
+    })
+    .catch( error => {
+      res.status(500).json({message: 'Server error', error})
+    })
+}
+
+function validateProject(req, res, next){
+  const { name, description, completed } = req.body
+  if(name && description){
+    // completed is optional and defaults to false if not provided
+    if(completed){
+      req.newProject = { name, description, completed: completed === true ? 1 : 0 }
+    } else {
+      req.newProject = { name, description, completed: 0 }
+    }
+    next()
+  } else {
+    res.status(400).json({message: 'Project name and description are required'})
+  }
+}
+
+router.get('/', (_, res) => {
+  projects.get()
+    .then( projects => {
+      res.status(200).json(projects)
+    })
+    .catch( error => {
+      res.status(500).json({message: 'Server error', error})
+    })
+})
+
+router.get('/:id', validiateProjectId, (req, res) => {
+  res.status(200).json(req.project)
+})
+
+router.post('/', validateProject, (req, res) => {
+  projects.insert(req.newProject)
+    .then( project => {
+      res.status(201).json(project)
+    })
+    .catch( error => {
+      console.log({message: 'Server error', error})
+    })
+})
+
+router.put('/:id', validateProject, validiateProjectId, (req, res) => {
+  projects.update(req.params.id, req.newProject)
+    .then( updated => {
+      res.status(200).json(updated)
+    })
+    .catch( error => {
+      res.status(500).json({message: 'Server error', error})
+    })
+})
+
+router.delete('/:id', validiateProjectId, (req, res) => {
+  projects.remove(req.params.id)
+    .then( () => {
+      res.status(204).json() // unfortunatly the readme says not to return :(
+      // that's also why I'm using 204 instead of 200
+
+      // if I did return it would be res.stauts(200).json(req.project)
+    })
+    .catch( error => {
+      res.status(500).json({message: 'Server error', error})
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const helmet = require('helmet')
+
+const projectsRouter = require('./projects/projects-router')
 const server = express();
+server.use(express.json())
+server.use(helmet()) // saw helmet in package.json so thought I'd add it
+server.use('/api/projects', projectsRouter)
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
